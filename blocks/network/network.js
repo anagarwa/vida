a7dc28808429d0198be3ec9743216bcb336090dc
anagarwa <anagarwa@adobe.com>
2024-06-23 17:43:46 +0530

    first commit

---
+++ blocks/network/network.js
@@ -0,0 +1,47 @@
+
+
+function initializeMap() {
+    var map;
+    map = new MapmyIndia.Map('map', {
+        center: [28.6139, 77.2090], // Default to New Delhi
+        zoom: 12
+    });
+}
+
+function onMapMyIndiaLoad() {
+    initializeMap();
+
+    document.getElementById('cityDropdown').addEventListener('change', function() {
+        var selectedCity = this.value;
+        if (selectedCity) {
+            fetchServiceCenters(selectedCity);
+        }
+    });
+}
+
+async function checkMapmyIndiaLoaded() {
+    if (typeof MapmyIndia !== 'undefined' && typeof MapmyIndia.Map !== 'undefined') {
+        onMapMyIndiaLoad();
+    } else {
+        setTimeout(checkMapmyIndiaLoaded, 100);
+    }
+}
+
+export default async function decorate(block) {
+
+    block.innerHTML= '';
+    const containerDiv = document.createElement('div');
+    containerDiv.className = 'container';
+    block.appendChild(containerDiv);
+
+    const staticContent = document.createElement('div');
+    staticContent.className = 'static-content';
+    containerDiv.appendChild(staticContent);
+
+    const mapDiv = document.createElement('div');
+    mapDiv.className = 'map';
+    mapDiv.id = 'map';
+    containerDiv.appendChild(mapDiv);
+    checkMapmyIndiaLoaded();
+
+}
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import {loadScript, sampleRUM} from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+
+loadScript('https://apis.mapmyindia.com/advancedmaps/v1/29fe9e3a677a04e5ef17b6165b2d8cb6/map_load?v=3.0');
 
 // add more delayed functionality here
